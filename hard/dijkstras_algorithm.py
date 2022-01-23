e7e36d870a5403aaed6b1730221c6b7d1c81bdd9
nightowlish <oanaolf@gmail.com>
2022-01-23 16:11:36 +0200

    fixed djikstra's algorithm: modified implementation from regular queue to min priority queue

--- hard/dijkstras_algorithm.py
+++ hard/dijkstras_algorithm.py
@@ -1,4 +1,4 @@
-from collections import deque
+from heapq import heappush, heappop
 
 
 class Node:
@@ -6,14 +6,21 @@ class Node:
         self.node = node
         self.edges = edges
 
+    def __lt__(self, object):
+         return self.node < object.node
+
+    def __gt__(self, object):
+         return self.node > object.node
+
 
 def dijkstrasAlgorithm(start, edges):
     distances = [None for _ in edges]
     distances[start] = 0
     visited = set()
-    nodes_to_visit = deque([Node(start, edges[start])])
+    nodes_to_visit = []
+    heappush(nodes_to_visit, Node(start, edges[start]))
     while nodes_to_visit:
-        current_node = nodes_to_visit.popleft()
+        current_node = heappop(nodes_to_visit)
         if current_node.node in visited:
             continue
         visited.add(current_node.node)
@@ -21,10 +28,9 @@ def dijkstrasAlgorithm(start, edges):
             current_distance = distances[current_node.node] + edge[1]
             distances[edge[0]] = current_distance if distances[edge[0]] is None else min(current_distance, distances[edge[0]])
             destination_node = Node(edge[0], edges[edge[0]])
-            nodes_to_visit.append(destination_node)
+            heappush(nodes_to_visit, destination_node)
     result = []
     for index in range(len(edges)):
         distance = -1 if distances[index] is None else distances[index]
         result.append(distance)
     return result
-        
